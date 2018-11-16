12f8830d9603e7312d713cc4bf9f72e850b6ae15
es99 <engels.franca@gmail.com>
2018-11-16 02:35:10 -0300

    adicionado view e template base

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.tela_inicial, name = 'inicio')
+	path('', views.tela_inicial, name = 'inicio'),
+	path('pesquisa/', views.pesquisa, name = 'pesquisa'),
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -5,4 +5,8 @@ from django.utils import timezone
 # Create your views here.
 def tela_inicial(request):
 	codigos = Livros.objects.order_by('codigo')
-	return render(request, 'biblioteca/inicio.html', {'livrosCadastrados': codigos})
+	numero = len(Livros.objects.all())
+	return render(request, 'biblioteca/inicio.html', {'livrosCadastrados': codigos, 'numeroLivros': numero})
+
+def pesquisa(request):
+	return render(request, 'biblioteca/pesquisa.html', {})
